5357151d984d381fd0eca591b80311dc246a9b3a
pontifexrm <ron@pontifex.nz>
2025-11-29 10:51:56 +1300

    Added date helper to  center on NZ date from what ever the computer date is.

---
+++ RVParking/Data/DateHelper.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+
+namespace RVParking.Data
+{
+    public static class DateHelper
+    {
+        public static DateTime GetCurrentDateInNZ()
+        {
+            // Find the New Zealand time zone
+            TimeZoneInfo nzTimeZone = TimeZoneInfo.FindSystemTimeZoneById("New Zealand Standard Time");
+
+            // Convert from UTC to NZ time
+            DateTime utcNow = DateTime.UtcNow;
+            DateTime nzNow = TimeZoneInfo.ConvertTimeFromUtc(utcNow, nzTimeZone);
+
+            return nzNow;
+        }
+    }
+
+}
