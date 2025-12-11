68785c79fd78a25f010163897472abb9c4c0dc67
pontifexrm <ron@pontifex.nz>
2025-12-11 23:00:16 +1300

    Deployed Ver: 1.0.60 2025-12-12 Added AppLog viewer. and changed sitestats page to have dropdown accordian controls for each category

--- RVParking/Data/VisitStats.cs
+++ RVParking/Data/VisitStats.cs
@@ -4,9 +4,12 @@
     {
         public string Location { get; set; }
         public int Count24h { get; set; }
-
         public int CountWeek { get; set; }
         public int CountMonth { get; set; }
         public int CountYear { get; set; }
+        public int UniqueUsers24h { get; set; }
+        public int UniqueUsersWeek { get; set; }
+        public int UniqueUsersMonth { get; set; }
+        public int UniqueUsersYear { get; set; }
     }
 }
