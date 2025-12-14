839a1e4a96d51bf640ed1b238369257253e9eb3a
pontifexrm <ron@pontifex.nz>
2025-12-14 22:51:47 +1300

    Change version # Updated Prod DB and small change to AskUsFmUser re saving booking as depart date was wrong

--- RVParking/Migrations/20251214085851_AppParam.Designer.cs
+++ RVParking/Migrations/20251214085851_AppParam.Designer.cs
@@ -3,6 +3,7 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using RVParking.Data;
 
@@ -11,9 +12,11 @@ using RVParking.Data;
 namespace RVParking.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    partial class ApplicationDbContextModelSnapshot : ModelSnapshot
+    [Migration("20251214085851_AppParam")]
+    partial class AppParam
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
